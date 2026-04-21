388716253e73e8dfb37f339e4f1f3cc4abe12d9b
Android Studio <devandroid@ort>
2026-04-21 15:48:08 -0300

    mm

--- web/script.js
+++ web/script.js
@@ -4,17 +4,20 @@ const input = document.getElementById("pokemonNombre")
 async function getPokemonByName() {
     let name = input.value
 
-const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const data = await res.json();
 
         let pokemon = { nombre: data.name, imagen: data.sprites.front_default, tipos:  data.types.map(t => t.type.name).join(", "), peso: data.weight, altura: data.height }
         Object.entries(pokemon).forEach(([key,value]) => {
             if(key != "imagen"){
                 document.getElementById(key).innerHTML = value
-            }else{
+            }else if(key == imagen){
                 document.getElementById(key).src = value
+            }else if(pokemon == null){
+                document.getElementById("nombre").innerHTML = "No se encontro el pokemon"
             }
         })
+
 
 
 
