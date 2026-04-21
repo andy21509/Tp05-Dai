7a754a057ba6f06391048cebc45f1d7623590d02
Android Studio <devandroid@ort>
2026-04-21 15:22:18 -0300

    terminado back

--- web/script.js
+++ web/script.js
@@ -1,17 +1,18 @@
 const input = document.getElementById("pokemonNombre")
+    
 
 async function getPokemonByName() {
     let name = input.value
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const data = await res.json();
 
-
-        let pokemon = { nombre: data.name, imagen: data.sprites[0], tipos: data.types, peso: data.weight, altura: data.height }
-        pokemon.forEach(e => {
-            if(e.key != "imagen"){
-                document.getElementById(e.key).innerHTML = e.value
+        let pokemon = { nombre: data.name, imagen: data.sprites.front_default, tipos:  data.types.map(t => t.type.name).join(", "), peso: data.weight, altura: data.height }
+        Object.entries(pokemon).forEach(([key,value]) => {
+            if(key != "imagen"){
+                document.getElementById(key).innerHTML = value
             }else{
-                document.getElementById(e.key).src = e.value
+                document.getElementById(key).src = value
             }
         })
 
