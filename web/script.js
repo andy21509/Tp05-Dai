9f6e61b4030eec312b80e4a0a76ebab1be727941
LUCAS IOEL ROITMAN <49518218@ort>
2026-04-22 17:20:20 -0300

    pokedex terminada

--- web/script.js
+++ web/script.js
@@ -1,28 +1,31 @@
 const input = document.getElementById("pokemonNombre")
-    
+
 
 async function getPokemonByName() {
     let name = input.value
 
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const data = await res.json();
 
-        let pokemon = { nombre: data.name, imagen: data.sprites.front_default, tipos:  data.types.map(t => t.type.name).join(", "), peso: data.weight, altura: data.height }
-        Object.entries(pokemon).forEach(([key,value]) => {
-            if(key != "imagen"){
+    if (res.ok) {
+        const data = await res.json();
+
+        let pokemon = { nombre: data.name, imagen: data.sprites.front_default, tipos: data.types.map(t => t.type.name).join(", "), peso: "Lb: " + data.weight, altura: "Inch: " + data.height }
+        Object.entries(pokemon).forEach(([key, value]) => {
+            if (key != "imagen") {
                 document.getElementById(key).innerHTML = value
-            }else if(key == imagen){
+            } else {
                 document.getElementById(key).src = value
-            }else if(pokemon == null){
-                document.getElementById("nombre").innerHTML = "No se encontro el pokemon"
             }
         })
-
-
-
-
-
+    } else {
+        document.getElementById("nombre").innerHTML = "No se encontro ningun pokemon"
+        document.getElementById("imagen").src = ""
+        document.getElementById("tipos").innerHTML = ""
+        document.getElementById("peso").innerHTML = ""
+        document.getElementById("altura").innerHTML = ""
+        
     }
+}
 
 
 
